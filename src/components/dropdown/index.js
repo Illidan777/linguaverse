23e169e49ad14f37138095088c1a96a3b7b2613c
Illidan <gorbasenko245@gmail.com>
2025-03-14 09:12:23 +0200

    refactored dropdown component;

--- src/components/dropdown/index.js
+++ src/components/dropdown/index.js
@@ -1,73 +1,74 @@
 import ContextMenu, {ContextMenuItem} from "../menu";
 import {TransparentPrimaryButton} from "../button/style";
 import {FONT_SIZES, FONT_WEIGHTS, StyledText} from "../text";
-import React, {Children, useEffect, useState} from "react";
+import React, {useEffect, useMemo, useState} from "react";
 import useCSSVariables from "../../hook/useCSSVariables";
 import styled from "styled-components";
 import {DownArrowIcon} from "../icon";
 import {FlexRow} from "../layout/style";
 
 const Dropdown = ({children, onSelect}) => {
-    const [buttonColor] = useCSSVariables(["--lavanda-dark"])
+    const [buttonColor] = useCSSVariables(["--lavanda-dark"]);
     const [selectedItemLabel, setSelectedItemLabel] = useState('');
 
-    useEffect(() => {
-        if(!selectedItemLabel) {
-            let defaultSelectedLabel = ''
-            Children.forEach(children, child => {
-                const { selected, label, disabled } = child.props;
-                if (selected) {
-                    defaultSelectedLabel = label
-                }
-            })
-            if(!defaultSelectedLabel) {
-                const firstChild = React.Children.toArray(children)[0]
-                const { label } = firstChild.props;
-                defaultSelectedLabel = label
+    const defaultSelectedLabel = useMemo(() => {
+        const childrenArray = React.Children.toArray(children);
+        let defaultLabel = '';
+
+        for (let child of childrenArray) {
+            const {selected, label} = child.props;
+            if (selected) {
+                defaultLabel = label;
+                break;
             }
-            setSelectedItemLabel(defaultSelectedLabel)
         }
-    }, [])
+
+        if (!defaultLabel && childrenArray.length > 0) {
+            defaultLabel = childrenArray[0].props.label;
+        }
+
+        return defaultLabel;
+    }, [children]);
+
+    useEffect(() => {
+        setSelectedItemLabel(defaultSelectedLabel);
+    }, [defaultSelectedLabel]);
 
     const handleSelect = (childProps) => {
         const {label, disabled} = childProps;
 
-        if(!disabled) {
+        if (!disabled) {
             setSelectedItemLabel(label);
             onSelect(childProps);
         }
-    }
+    };
 
     return (
-        <ContextMenu
-            alignRight
-            trigger={
-                <TransparentPrimaryButton
-                >
-                    <FlexRow gap="10px" align="center">
-                        <StyledText
-                            as="span"
-                            size={FONT_SIZES.SIMPLE_SMALL}
-                            weight={FONT_WEIGHTS.SEMI_BOLD}
-                            color={buttonColor}
-                        >
-                            {selectedItemLabel}
-                        </StyledText>
-                        <DownArrowIcon color={buttonColor} size="15px"/>
-                    </FlexRow>
-                </TransparentPrimaryButton>
-            }
-        >
-            {Children.map(children, child =>
-                React.isValidElement(child) ? (
-                    <div onClick={() => handleSelect(child.props)}>
-                        {child}
-                    </div>
-                ) : null
+        <ContextMenu alignRight trigger={
+            <TransparentPrimaryButton>
+                <FlexRow gap="10px" align="center">
+                    <StyledText
+                        as="span"
+                        size={FONT_SIZES.SIMPLE_SMALL}
+                        weight={FONT_WEIGHTS.SEMI_BOLD}
+                        color={buttonColor}
+                    >
+                        {selectedItemLabel}
+                    </StyledText>
+                    <DownArrowIcon color={buttonColor} size="15px"/>
+                </FlexRow>
+            </TransparentPrimaryButton>
+        }>
+            {React.Children.map(children, child =>
+                    React.isValidElement(child) && (
+                        <div onClick={() => handleSelect(child.props)}>
+                            {child}
+                        </div>
+                    )
             )}
         </ContextMenu>
-    )
-}
+    );
+};
 
 export const DropDownItem = styled(ContextMenuItem)``
 
